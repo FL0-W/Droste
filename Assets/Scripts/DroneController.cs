98bae2c5b75df960405ba92e13ee1fd9bbb07cf9
fbalfouong <florentin.balfouong@gipatgeri.fr>
2022-12-27 23:54:25 +0100

    Drone stabilization
    + assign location (coordinates x,z)
    + update prefab
    + fixed bugs

--- Assets/Scripts/DroneController.cs
+++ Assets/Scripts/DroneController.cs
@@ -9,67 +9,156 @@ public class DroneController : MonoBehaviour
     //Drone components
     public GameObject core;
     private List<IRotor> _rotors = new List<IRotor>();
-    
+
     //Drone physics
-    private Rigidbody rb;
-    private float minMaxPitch = 30;
-    private float minMaxRoll = 30;
+    private Rigidbody rb_core;
+    private float minMaxPitch = 5;
+    private float minMaxRoll = 5;
     private float pitchComputed;
     private float rollComputed;
     private float yawComputed;
     private float yaw;
     private float yawPower = 4;
-    private float lerpSpeed = 2;
+    private float lerpSpeed = 1;
+    Vector3 moving = new Vector3(0f, 0f, 0f);
 
     //Other variables
     private bool liftCompleted;
+    private float xDistanceFromStart;
+    private float zDistanceFromStart;
+
+
+    //Debug
+    public bool debug = false;
 
     // Start is called before the first frame update
     void Start()
     {
         liftCompleted = false;
-        rb = core.GetComponent<Rigidbody>();
+        rb_core = core.GetComponent<Rigidbody>();
         _rotors = GetComponentsInChildren<IRotor>().ToList();
     }
 
     void Update()
     {
-        if(!liftCompleted && rb.position.y >= 10){
+        if(!liftCompleted && rb_core.position.y >= 10){
             liftCompleted = true;
-            // SideMotions(1, 0, 1);
         }
     }
 
     void FixedUpdate()
     {
-        if (!rb)
+        if (!rb_core)
         {
             return;
         }
         HandleRotors();
+
+        //Action aftre setup
+        if(liftCompleted){
+            //SideMotions(0, 1, 0);
+            AssignObjective(0,0);
+            GoToAndStabilize(0, 0);
+        }
     }
 
     public void HandleRotors()
     {
         foreach (IRotor rotor in _rotors)
         {
-            rotor.UpdateRotor(rb);
+            rotor.UpdateRotor(rb_core);
         }
     }
-    
-    public void SideMotions(float yAxis = 0f, float xAxis = 0, float rotationA = 0f)
-    {
-        float pitchValue = yAxis * minMaxPitch;
-        float rollValue = -xAxis * minMaxRoll;
-        yaw += rotationA * yawPower;
 
-        pitchComputed = Mathf.Lerp(pitchComputed, pitchValue, lerpSpeed * Time.deltaTime);
-        rollComputed = Mathf.Lerp(rollComputed, rollValue, lerpSpeed * Time.deltaTime);
-        yawComputed = Mathf.Lerp(yawComputed, yaw, lerpSpeed * Time.deltaTime);
-            
-        Quaternion rot = Quaternion.Euler(pitchComputed,yawComputed,rollComputed);
-        rb.MoveRotation(rot);
+    public void SideMotions(float xAxis = 0f, float zAxis = 0f, float rotationA = 0f)
+    {
+            float pitchValue = zAxis * minMaxPitch;
+            float rollValue = -xAxis * minMaxRoll;
+            yaw += rotationA * yawPower;
+
+            pitchComputed = Mathf.Lerp(pitchComputed, pitchValue, lerpSpeed * Time.deltaTime);
+            rollComputed = Mathf.Lerp(rollComputed, rollValue, lerpSpeed * Time.deltaTime);
+            yawComputed = Mathf.Lerp(yawComputed, yaw, lerpSpeed * Time.deltaTime);
+
+            Quaternion rot = Quaternion.Euler(pitchComputed,yawComputed,rollComputed);
+            rb_core.MoveRotation(rot);
     }
 
+    public void GoToAndStabilize(float xAxis, float zAxis)
+    {
+        float x = 0f;
+        float z = 0f;
+        float r = 0f;
+
+        if(xAxis < rb_core.position.x){
+            if(rb_core.position.x - xAxis < 1){
+                x = StabilizeX();
+            }else{
+                x = -1 * ((rb_core.position.x - xAxis ) / xDistanceFromStart);
+            }
+
+            if(!debug){
+                print("xDistanceFromStart = "+xDistanceFromStart+" ; xAxis = "+xAxis+" ; rb_core.position.x = "+rb_core.position.x+ "; calcul = "+x);
+            }
+
+        }
+        else if(xAxis > rb_core.position.x){
+            if(xAxis - rb_core.position.x < 1){
+                x = StabilizeX();
+            }else{
+                x = 1 * ((xAxis - rb_core.position.x) / xDistanceFromStart);
+            }
+        }
+        
+
+
+        if(zAxis < rb_core.position.z){
+            if(rb_core.position.z - zAxis < 1){
+                z = StabilizeZ();
+            }else{
+                z = -1 * ((rb_core.position.z - zAxis ) / zDistanceFromStart);
+            }
+        }
+        else if(zAxis > rb_core.position.z){
+            if(zAxis - rb_core.position.z < 1){
+                z = StabilizeZ();
+            }else{
+                z = 1 * ((zAxis - rb_core.position.z) / zDistanceFromStart);
+            }
+        }
+        
+
+        if(!debug){
+            print("X Z R"+x+" "+z+" "+r);
+            debug =true;
+        }
+        SideMotions(x, z, r);
+    }
+
+    public float StabilizeX()
+    {
+        return -rb_core.velocity.x * 3;
+    }
+
+    public float StabilizeZ()
+    {
+        return -rb_core.velocity.z * 3;
+    }
+
+    public void AssignObjective(float x, float z)
+    {
+        //Assign distance on xAxis
+        if(rb_core.position.x > x){
+            xDistanceFromStart = rb_core.position.x - x;
+        }else{
+            xDistanceFromStart = x - rb_core.position.x;
+        }
+        //Assign distance on zAxis
+        if(rb_core.position.z > z){
+            zDistanceFromStart = rb_core.position.z - z;
+        }else{
+            zDistanceFromStart = z - rb_core.position.z;
+        }
+    }
 
 }
--- Assets/Scripts/Rotor/RotorController.cs
+++ Assets/Scripts/Rotor/RotorController.cs
@@ -7,35 +7,50 @@ using Scripts.Rotor;
 public class RotorController : MonoBehaviour, IRotor
 {
 
-    private float maxPower = 9.9f;
+    private float maxPower = 4f;
     private float throttle = 1;
+    private float minLimitUp = 9;
+    private float maxLimitUp = 10;
 
-    public void UpdateRotor(Rigidbody rb)
+
+    private Rigidbody rb;
+
+    public void UpdateRotor(Rigidbody rbody)
     {
+        rb = rbody;
         Vector3 upVector = transform.up;
         upVector.x = 0;
         upVector.z = 0;
         float diff = 1 - upVector.magnitude;
-        float finalDiff = diff * Physics.gravity.magnitude;
-        
-        if(rb.transform.position.y < 9){
-            IncreasePower();
-        }
-        if(rb.transform.position.y > 10){
-            DecreasePower();
-        }
+        float finalDiff = diff * Physics.gravity.magnitude;        
+
+        UpdateThrottle();
         Vector3 engineForce = Vector3.zero;
         engineForce = transform.up * ((rb.mass*Physics.gravity.magnitude + finalDiff ) + (throttle * maxPower))/4;
         rb.AddForce(engineForce,ForceMode.Force);
     }
 
-    public void DecreasePower()
+    public void DecreaseThrottle()
     {
-        maxPower = 9f;
+        throttle = (-1f * ((rb.position.y - maxLimitUp )) / maxLimitUp) / maxPower;
     }
 
-    public void IncreasePower()
+    public void IncreaseThrottle()
     {
-        maxPower = 10f;
+        throttle = (1f * ((minLimitUp - rb.position.y)) / minLimitUp) / maxPower;
+    }
+
+    public void UpdateThrottle()
+    {
+        if(rb.position.y < minLimitUp){
+            IncreaseThrottle();
+        }
+        else if(rb.position.y > maxLimitUp){
+            DecreaseThrottle();
+        }else{
+            //Brievely reverse ongoing motion
+            throttle = -rb.velocity.y;
+        }
+
     }
 }
